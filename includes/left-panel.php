3ff90a6368cc74bf53ddb15994d27036479c9649
NadunKulatunge <nadunku.16@cse.mrt.ac.lk>
2018-03-04 23:57:00 +0530

    added more options

--- includes/left-panel.php
+++ includes/left-panel.php
@@ -1,52 +1,55 @@
 <?php if($_SESSION['userRole']=="Parent"){
 	echo'<div class="col-md-3 wthree_footer_grid_left1 ">
 				<ul class="nav nav-pills nav-stacked">
-					<li role="presentation" class="box"><a href="#">Profile</a></li>
-					<li role="presentation" class="box"><a href="#">TimeTable</a></li>
-					<li role="presentation" class="box"><a href="#">Student Attendance</a></li>
-					<li role="presentation" class="box"><a href="#">Leave Request</a></li>
-					<li role="presentation" class="box"><a href="#">Payments</a></li>
-					<li role="presentation" class="box"><a href="#">Messages</a></li>					
-					<li role="presentation" class="box"><a href="#">Appointments</a></li>
+					<li role="presentation" class="box"><a href="profile.php">Profile</a></li>
+					<li role="presentation" class="box"><a href="timetable.php">TimeTable</a></li>
+					<li role="presentation" class="box"><a href="attendance.php">Student Attendance</a></li>
+					<li role="presentation" class="box"><a href="leave.php">Leave Request</a></li>
+					<li role="presentation" class="box"><a href="payments.php">Payments</a></li>
+					<li role="presentation" class="box"><a href="messages.php">Messages</a></li>					
+					<li role="presentation" class="box"><a href="appointments.php">Appointments</a></li>
 					<li role="presentation" class="box"><a href="logout.php">Log Out</a></li>
 				</ul>
 			</div>';
 }elseif($_SESSION['userRole']=="Teacher"){
 	echo'<div class="col-md-3 wthree_footer_grid_left1 ">
 				<ul class="nav nav-pills nav-stacked">
-					<li role="presentation" class="box"><a href="#">Profile</a></li>
-					<li role="presentation" class="box"><a href="#">TimeTable</a></li>
-					<li role="presentation" class="box"><a href="#">Student Attendance</a></li>
-					<li role="presentation" class="box"><a href="#">Leave Request</a></li>
-					<li role="presentation" class="box"><a href="#">Child Details</a></li>
-					<li role="presentation" class="box"><a href="#">Messages</a></li>					
-					<li role="presentation" class="box"><a href="#">Appointments</a></li>
+					<li role="presentation" class="box"><a href="profile.php">Profile</a></li>
+					<li role="presentation" class="box"><a href="timetable.php">TimeTable</a></li>
+					<li role="presentation" class="box"><a href="attendance.php">Student Attendance</a></li>
+					<li role="presentation" class="box"><a href="leave.php">Leave Request</a></li>
+					<li role="presentation" class="box"><a href="payments.php">Payments</a></li>
+					<li role="presentation" class="box"><a href="childdetails.php">Child Details</a></li>
+					<li role="presentation" class="box"><a href="messages.php">Messages</a></li>					
+					<li role="presentation" class="box"><a href="appointments.php">Appointments</a></li>
 					<li role="presentation" class="box"><a href="logout.php">Log Out</a></li>
 				</ul>
 			</div>';
 }elseif($_SESSION['userRole']=="Principal"){
 	echo'<div class="col-md-3 wthree_footer_grid_left1 ">
 				<ul class="nav nav-pills nav-stacked">
-					<li role="presentation" class="box"><a href="#">Profile</a></li>
-					<li role="presentation" class="box"><a href="#">TimeTable</a></li>
-					<li role="presentation" class="box"><a href="#">Student Attendance</a></li>
-					<li role="presentation" class="box"><a href="#">Leave Request</a></li>
-					<li role="presentation" class="box"><a href="#">Enrollments</a></li>
-					<li role="presentation" class="box"><a href="#">Child Details</a></li>
-					<li role="presentation" class="box"><a href="#">Messages</a></li>					
-					<li role="presentation" class="box"><a href="#">Appointments</a></li>
+					<li role="presentation" class="box"><a href="profile.php">Profile</a></li>
+					<li role="presentation" class="box"><a href="timetable.php">TimeTable</a></li>
+					<li role="presentation" class="box"><a href="attendance.php">Student Attendance</a></li>
+					<li role="presentation" class="box"><a href="leave.php">Leave Request</a></li>
+					<li role="presentation" class="box"><a href="payments.php">Payments</a></li>
+					<li role="presentation" class="box"><a href="enrollments.php">Enrollments</a></li>
+					<li role="presentation" class="box"><a href="childdetails.php">Child Details</a></li>
+					<li role="presentation" class="box"><a href="messages.php">Messages</a></li>					
+					<li role="presentation" class="box"><a href="appointments.php">Appointments</a></li>
 					<li role="presentation" class="box"><a href="logout.php">Log Out</a></li>
 				</ul>
 			</div>';
 }elseif($_SESSION['userRole']=="Admin"){
 	echo'<div class="col-md-3 wthree_footer_grid_left1 ">
 				<ul class="nav nav-pills nav-stacked">
-					<li role="presentation" class="box"><a href="#">Profile</a></li>
-					<li role="presentation" class="box"><a href="#">TimeTable</a></li>
-					<li role="presentation" class="box"><a href="#">Student Attendance</a></li>
-					<li role="presentation" class="box"><a href="#">Enrollments</a></li>
-					<li role="presentation" class="box"><a href="#">Child Details</a></li>
-					<li role="presentation" class="box"><a href="#">Messages</a></li>					
+					<li role="presentation" class="box"><a href="profile.php">Profile</a></li>
+					<li role="presentation" class="box"><a href="timetable.php">TimeTable</a></li>
+					<li role="presentation" class="box"><a href="attendance.php">Student Attendance</a></li>
+					<li role="presentation" class="box"><a href="payments.php">Payments</a></li>
+					<li role="presentation" class="box"><a href="enrollments.php">Enrollments</a></li>
+					<li role="presentation" class="box"><a href="childdetails.php">Child Details</a></li>
+					<li role="presentation" class="box"><a href="messages.php">Messages</a></li>					
 					<li role="presentation" class="box"><a href="logout.php">Log Out</a></li>
 				</ul>
 			</div>';
