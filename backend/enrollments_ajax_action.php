5ddcdbf60279517e73eccec6a296615d2f487c27
NadunKulatunge <nadunku.16@cse.mrt.ac.lk>
2018-04-21 22:42:52 +0530

    Enrollment validations

--- backend/enrollments_ajax_action.php
+++ backend/enrollments_ajax_action.php
@@ -1,18 +1,37 @@
  <?php
  session_start();
- if(isset($_POST["action"]) && ($_SESSION['userRole']== "Principal" || $_SESSION['userRole']== "Admin") )
- {  
+ require_once 'validator_functions.php';
+ $Err = "";
+
+ if(isset($_POST["action"]) && ($_SESSION['userRole']== "Principal" || $_SESSION['userRole']== "Admin") ) {
       $output = '';  
        include_once($_SERVER['DOCUMENT_ROOT'].'/KidsCave/backend/dbconfig.php');
-      if($_POST["action"] =="Add")  
-      {
-            $name = mysqli_real_escape_string($connect, $_POST["name"]);
-            $email = mysqli_real_escape_string($connect, $_POST["email"]);
-            $role = mysqli_real_escape_string($connect, $_POST["role"]);
-            $gender = mysqli_real_escape_string($connect, $_POST["gender"]);
-            $birthday = mysqli_real_escape_string($connect, $_POST["birthday"]);
-            $phone = mysqli_real_escape_string($connect, $_POST["phone"]);
-            $classroom = mysqli_real_escape_string($connect, $_POST["classroom"]);
+      if($_POST["action"] =="Add") {
+
+          $name = test_input($_POST["name"]);
+          $email = test_input($_POST["email"]);
+          $role = test_input($_POST["role"]);
+          $gender = test_input($_POST["gender"]);
+          $birthday = test_input($_POST["birthday"]);
+          $phone = test_input($_POST["phone"]);
+          $classroom = test_input($_POST["classroom"]);
+
+          //validate
+          if(!valid_name($name)){
+              $Err = "Invalid name";
+          }elseif(!valid_email($email)){
+              $Err = "Invalid email";
+          }elseif(!valid_role($role)){
+              $Err = "Invalid role";
+          }elseif(!valid_gender($gender)){
+              $Err = "Invalid gender";
+          }elseif(!valid_date($birthday)){
+              $Err = "Invalid birthday";
+          }elseif(!valid_phone($phone)){
+              $Err = "Invalid phone";
+          }elseif(!valid_classroom((int)$classroom,$connect)){
+              $Err = "Invalid classroom";
+          }
 
            $procedure = "  
                 CREATE PROCEDURE insertUser(IN name varchar(100), email varchar(100), role varchar(9), gender varchar(6), birthday varchar(10), phone varchar(12), classroom int(11) )  
@@ -20,40 +39,62 @@
                     INSERT INTO tbl_users(userName, userEmail, userRole, classID, gender, birthday, userPhone) VALUES (name, email, role, classroom, gender, birthday, phone);
                 END;  
            ";  
-           if(mysqli_query($connect, "DROP PROCEDURE IF EXISTS insertUser"))
-           {  
-                if(mysqli_query($connect, $procedure))  
-                {  
-                    $query = "CALL insertUser('".$name."', '".$email."', '".$role."', '".$gender."', '".$birthday."', '".$phone."', '".$classroom."')";
-                     mysqli_query($connect, $query);
-                      echo 'New user created';
-                }  
+           if(mysqli_query($connect, "DROP PROCEDURE IF EXISTS insertUser")) {
+
+               if($Err=="") {
+                   if (mysqli_query($connect, $procedure)) {
+                       $query = "CALL insertUser('" . $name . "', '" . $email . "', '" . $role . "', '" . $gender . "', '" . $birthday . "', '" . $phone . "', '" . $classroom . "')";
+                       mysqli_query($connect, $query);
+                       echo 'New user created';
+                   }
+               }else{
+                    echo $Err;
+                    $Err= "";
+                }
            }  
       }
-      if($_POST["action"] == "Edit")  
-      {  
-            $name = mysqli_real_escape_string($connect, $_POST["name"]);
-            $email = mysqli_real_escape_string($connect, $_POST["email"]);
-            $role = mysqli_real_escape_string($connect, $_POST["role"]);
-            $gender = mysqli_real_escape_string($connect, $_POST["gender"]);
-            $birthday = mysqli_real_escape_string($connect, $_POST["birthday"]);
-            $phone = mysqli_real_escape_string($connect, $_POST["phone"]);
-            $classroom = mysqli_real_escape_string($connect, $_POST["classroom"]);
+      if($_POST["action"] == "Edit") {
+          $name = test_input($_POST["name"]);
+          $email = test_input($_POST["email"]);
+          $role = test_input($_POST["role"]);
+          $gender = test_input($_POST["gender"]);
+          $birthday = test_input($_POST["birthday"]);
+          $phone = test_input($_POST["phone"]);
+          $classroom = test_input($_POST["classroom"]);
 
+            //validate
+          if(!valid_name($name)){
+              $Err = "Invalid name";
+          }elseif(!valid_email($email)){
+              $Err = "Invalid email";
+          }elseif(!valid_role($role)){
+              $Err = "Invalid role";
+          }elseif(!valid_gender($gender)){
+              $Err = "Invalid gender";
+          }elseif(!valid_date($birthday)){
+              $Err = "Invalid birthday";
+          }elseif(!valid_phone($phone)){
+              $Err = "Invalid phone";
+          }elseif(!valid_classroom((int)$classroom,$connect)){
+              $Err = "Invalid classroom";
+          }
            $procedure = "  
                 CREATE PROCEDURE updateUser(IN ID int(11), name varchar(100), email varchar(100), role varchar(9), gender varchar(6), birthday varchar(10), phone varchar(12), classroom int(11) )  
                 BEGIN
                     UPDATE tbl_users SET userName = name, userEmail = email, userRole = role, classID = classroom, gender = gender, birthday = birthday, userPhone = phone WHERE userID = ID;        
                 END;   
            ";
-           if(mysqli_query($connect, "DROP PROCEDURE IF EXISTS updateUser"))
-           {  
-                if(mysqli_query($connect, $procedure))  
-                {  
-                    $query = "CALL updateUser('".$_POST["id"]."', '".$name."', '".$email."', '".$role."', '".$gender."', '".$birthday."', '".$phone."', '".$classroom."')";
-                     mysqli_query($connect, $query);  
-                     echo 'Data Updated';  
-                }  
+           if(mysqli_query($connect, "DROP PROCEDURE IF EXISTS updateUser")) {
+               if($Err=="") {
+                   if (mysqli_query($connect, $procedure)) {
+                       $query = "CALL updateUser('" . $_POST["id"] . "', '" . $name . "', '" . $email . "', '" . $role . "', '" . $gender . "', '" . $birthday . "', '" . $phone . "', '" . $classroom . "')";
+                       mysqli_query($connect, $query);
+                       echo 'Data Updated';
+                   }
+               } else {
+                   echo $Err;
+                   $Err= "";
+               }
            }  
       }  
       if($_POST["action"] == "Disapprove" && $_SESSION['userRole']== "Admin")
